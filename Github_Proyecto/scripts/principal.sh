5b26db28426dcac24514833d3908e7cfb2231896
anyatorres70 <anyatorres70@gmail.com>
2026-05-06 21:14:41 -0600

    actualizacion de scripts

--- Github_Proyecto/scripts/principal.sh
+++ Github_Proyecto/scripts/principal.sh
@@ -3,9 +3,9 @@
 #Anya Miranda 
 #script que junta diferentes source para poder descargar una secuencia genomica por genes, separar cada secuencia en codones, marcar codones especificos (como CTG), contar la cantidad de codones que tiene cada uno y ordenarlos de mayor a menor cantidad de codones
 
-# ==========================
+# ================
 # Ruta del script
-# ==========================
+# ================
 script_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # ==========================
@@ -15,15 +15,15 @@ source "$script_DIR/config.sh"
 source "$script_DIR/../.env"
 source "$script_DIR/telegram.sh"
 
-# ==========================
-# Inicio pipeline
-# ==========================
+# =================
+# Inicio analisis
+# =================
 echo "Iniciando analisis completo"
 send_telegram "Iniciando analisis completo"
 
-# ==========================
+# ==================
 # Paso 1: Descarga
-# ==========================
+# ==================
 echo "Paso 1: Descarga RefSeq"
 bash "$script_DIR/refseq.sh"
 
@@ -33,9 +33,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# ==========================
+# ======================
 # Paso 2: Análisis CTG
-# ==========================
+# ======================
 echo "Paso 2: Análisis CTG"
 bash "$script_DIR/analisis_ctg.sh"
 
@@ -45,8 +45,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# ==========================
+# ==============
 # Fin analisis
-# ==========================
+# ==============
 echo "Analisis completado correctamente"
 send_telegram "Analisis completado con éxito"
